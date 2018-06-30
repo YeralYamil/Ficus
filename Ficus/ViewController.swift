0099ca21c58798355ef86f7cb097810534fc9694
Yeral <YYamil24@gmail.com>
2018-06-30 11:48:43 -0400

    Initial commit

---
+++ Ficus/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ficus
+//
+//  Created by Yeral Yamil Castillo on 6/29/18.
+//  Copyright © 2018 Yeral Yamil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
