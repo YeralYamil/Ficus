8c630dd170f2194f184d562694555e36431ee515
YYamil <YYamil24@gmail.com>
2018-07-16 18:52:14 -0400

    Add classes for the calculator screen

---
+++ Ficus/View/ViewControllers/CalculatorTableViewController.swift
@@ -0,0 +1,37 @@
+//
+//  CalculatorViewController.swift
+//  Ficus
+//
+//  Created by Yeral Yamil on 7/15/18.
+//  Copyright © 2018 Yeral Yamil. All rights reserved.
+//
+
+import UIKit
+
+class CalculatorTableViewController: UITableViewController {
+    
+    var calculatorViewModel: CalculatorViewModel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Ficus/ViewModel/Adapters/CarPickerAdapter.swift
@@ -0,0 +1,9 @@
+//
+//  CarPickerAdapter.swift
+//  Ficus
+//
+//  Created by Yeral Yamil on 7/15/18.
+//  Copyright © 2018 Yeral Yamil. All rights reserved.
+//
+
+import Foundation
